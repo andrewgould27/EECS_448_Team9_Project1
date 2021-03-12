a19be29ffa7ab462e1ddfe87ab74f81d4711841a
Yuelin Xie <74153760+903481310@users.noreply.github.com>
2021-03-11 20:06:14 -0600

    Add files via upload

--- gameModeConfig.js
+++ gameModeConfig.js
@@ -4,6 +4,22 @@
  * @author Team 9
  */
 
+function background_music(src){
+    this.background_music=document.createElement('audio')
+    this.background_music.setAttribute('id', 'music')
+    this.background_music.src=src
+    this.background_music.setAttribute('preload','auto')
+    this.background_music.style.display='none'
+    document.body.appendChild(this.background_music)
+    this.play=function(){
+        this.background_music.play()
+        let x=document.getElementById('music')
+        x.loop=true
+    }
+    this.stop=function(){
+        this.background_music.pause()
+    }
+}
 /**
  * Configures variables for game counters and win conditions and removes configuration buttons. 
  * Loads the ship selection screen
@@ -12,8 +28,12 @@
  */
 function main(gameType)
 {
+    
+
     if(gameType.id == 'local')
     {
+        game_music=new background_music('background.mp3')
+        game_music.play()
         const localGameScript = document.createElement('script');
         localGameScript.type = 'text/javascript';
         localGameScript.src = 'againstLocalPlayer.js';
@@ -22,6 +42,8 @@ function main(gameType)
     }
     else if(gameType.id === 'botGame')
     {
+        game_music=new background_music('background.mp3')
+        game_music.play()
         const botGameScript = document.createElement('script');
         botGameScript.type = 'text/javascript';
         botGameScript.src = 'againstBot.js';
